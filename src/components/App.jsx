190bf2c731d16e808538ea019b0bdf15a8f77de8
Nadia Zubko <ne.nadezda@gmail.com>
2022-09-08 22:13:48 -0600

    Set state with fetched data, added List component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
+/* eslint-disable import/prefer-default-export */
 import './App.css';
+import { useState } from 'react';
 
 import { searchArtworks } from '../utils/api';
 import { SearchForm } from './SearchForm';
+import { List } from './List';
 import { Footer } from './Footer';
 
 export function App() {
+	const [data, setData] = useState('');
+
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -13,7 +24,7 @@ export function App() {
 		// our UI, we need to make real requests!
 		// @see: ./src/uitls/api.js
 		searchArtworks(query).then((json) => {
-			console.log(json);
+			setData(json.data);
 		});
 	}
 
@@ -21,6 +32,7 @@ export function App() {
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
+			<List data={data} />
 			<Footer />
 		</div>
 	);
