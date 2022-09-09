6afe7362d4bbe52009ddc21b5c73ae2a525b6833
Nadia Zubko <ne.nadezda@gmail.com>
2022-09-08 22:14:11 -0600

    Added rules for eslint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
+/* eslint-disable import/prefer-default-export */
 import './Footer.css';
 
-export const Footer = () => (
-	<footer className="Footer">
-		<p>
-			Artwork and all related information courtesy of{' '}
-			<a href="https://www.artic.edu/">Art Institute of Chicago</a>.
-		</p>
-	</footer>
-);
+export function Footer() {
+	return (
+		<footer className="Footer">
+			<p>
+				Artwork and all related information courtesy of{' '}
+				<a href="https://www.artic.edu/">Art Institute of Chicago</a>.
+			</p>
+		</footer>
+	);
+}
